d698281ee2e696ad78e23cb3c849876cc4c02238
Rick South <rick@natamus.com>
2023-12-11 23:56:56 +0100

    TNT Breaks Bedrock source code for 1.20.4.

---
+++ NeoForge/src/main/java/com/natamus/tntbreaksbedrock/ModNeoForge.java
@@ -0,0 +1,32 @@
+package com.natamus.tntbreaksbedrock;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.tntbreaksbedrock.neoforge.events.NeoForgeBoomEvent;
+import com.natamus.tntbreaksbedrock.util.Reference;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(Reference.MOD_ID)
+public class ModNeoForge {
+	
+	public ModNeoForge() {
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		modEventBus.addListener(this::loadComplete);
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+		NeoForge.EVENT_BUS.register(NeoForgeBoomEvent.class);
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
