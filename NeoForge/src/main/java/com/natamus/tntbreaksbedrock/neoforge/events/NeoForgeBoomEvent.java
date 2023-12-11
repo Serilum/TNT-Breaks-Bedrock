4c721f9ebd7581c7014c92dcc456fabd9dfaeccb
Rick South <rick@natamus.com>
2023-12-11 23:56:47 +0100

    TNT Breaks Bedrock source code for 1.20.2.

---
+++ NeoForge/src/main/java/com/natamus/tntbreaksbedrock/neoforge/events/NeoForgeBoomEvent.java
@@ -0,0 +1,16 @@
+package com.natamus.tntbreaksbedrock.neoforge.events;
+
+import com.natamus.tntbreaksbedrock.events.BoomEvent;
+import net.minecraft.world.level.Explosion;
+import net.neoforged.neoforge.event.level.ExplosionEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeBoomEvent {
+	@SubscribeEvent
+	public static void onExplosion(ExplosionEvent.Detonate e) {
+		Explosion explosion = e.getExplosion();
+		BoomEvent.onExplosion(e.getLevel(), explosion.getExploder(), explosion);
+	}
+}
