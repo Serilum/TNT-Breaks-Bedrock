043776bf3d0965d3faa4db6580b7959f77f04556
Rick South <rick@natamus.com>
2024-07-28 07:12:23 +0200

    (v3.4) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Fabric/src/main/java/com/natamus/tntbreaksbedrock/ModFabric.java
+++ Fabric/src/main/java/com/natamus/tntbreaksbedrock/ModFabric.java
@@ -1,6 +1,7 @@
 package com.natamus.tntbreaksbedrock;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.collective.fabric.callbacks.CollectiveExplosionEvents;
 import com.natamus.tntbreaksbedrock.events.BoomEvent;
 import com.natamus.tntbreaksbedrock.util.Reference;
@@ -13,6 +14,10 @@ public class ModFabric implements ModInitializer {
 	
 	@Override
 	public void onInitialize() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		setGlobalConstants();
 		ModCommon.init();
 
