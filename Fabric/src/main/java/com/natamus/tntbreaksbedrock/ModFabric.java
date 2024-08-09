fb4aa6dfcd3ac7fd8a274ec6492de421a284e6ab
Rick South <rick@natamus.com>
2024-08-09 08:03:16 +0200

    TNT Breaks Bedrock source code for 1.21.1.

---
+++ Fabric/src/main/java/com/natamus/tntbreaksbedrock/ModFabric.java
@@ -0,0 +1,38 @@
+package com.natamus.tntbreaksbedrock;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
+import com.natamus.collective.fabric.callbacks.CollectiveExplosionEvents;
+import com.natamus.tntbreaksbedrock.events.BoomEvent;
+import com.natamus.tntbreaksbedrock.util.Reference;
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Explosion;
+import net.minecraft.world.level.Level;
+
+public class ModFabric implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		loadEvents();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadEvents() {
+		CollectiveExplosionEvents.EXPLOSION_DETONATE.register((Level world, Entity sourceEntity, Explosion explosion) -> {
+			BoomEvent.onExplosion(world, sourceEntity, explosion);
+		});
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
