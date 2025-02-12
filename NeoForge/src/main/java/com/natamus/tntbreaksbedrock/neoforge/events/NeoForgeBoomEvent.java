b63526887114bbfe71e3aa296849337fd684d7be
Rick South <rick@natamus.com>
2025-02-12 23:10:44 +0100

    (v3.5) (Forge & Neo) Removed redundant event register annotations. [+1]

--- NeoForge/src/main/java/com/natamus/tntbreaksbedrock/neoforge/events/NeoForgeBoomEvent.java
+++ NeoForge/src/main/java/com/natamus/tntbreaksbedrock/neoforge/events/NeoForgeBoomEvent.java
@@ -4,9 +4,7 @@ import com.natamus.tntbreaksbedrock.events.BoomEvent;
 import net.minecraft.world.level.Explosion;
 import net.neoforged.neoforge.event.level.ExplosionEvent;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
 
-@EventBusSubscriber
 public class NeoForgeBoomEvent {
 	@SubscribeEvent
 	public static void onExplosion(ExplosionEvent.Detonate e) {
